2784e43c6073b4d783aa3390c3376b1d3dc0d738
sensei <qch_2@qq.com>
2026-09-12 11:40:11 +0800

    Fix syntax error in server.js

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ const path = require('path');
 // ---------- Express 应用 ----------
 const app = express();
 app.use(cors());
-app.use(express.json())；
+app.use(express.json());
 app.use(express.static(__dirname));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-
 
 // 健康检查
 app.get('/api/health', (req, res) => {
