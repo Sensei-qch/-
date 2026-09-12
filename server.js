314f47d0286c7c1616e34aaa9f1f3b0ea0339a2b
sensei <qch_2@qq.com>
2026-09-12 11:29:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ const path = require('path');
 // ---------- Express 应用 ----------
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())；
+app.use(express.static(__dirname));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
 
 // 健康检查
 app.get('/api/health', (req, res) => {
